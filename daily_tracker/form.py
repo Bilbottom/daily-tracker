6ad2779e05504b12cf6424ccb1ac5b6e04f9e42c
Bill <Bill@TheWallisFamily.co.uk>
2022-11-13 18:31:27 +0000

    Tweaks and updates

--- daily_tracker/form.py
+++ daily_tracker/form.py
@@ -1,15 +1,126 @@
 """
 The form for the pop-up box.
 """
+import datetime
 import tkinter
-import tkinter.ttk
+from typing import Callable
 
 
-def main() -> None:
-    """"""
-    root = tkinter.Tk()
-    frm = tkinter.ttk.Frame(root, padding=10)
-    frm.grid()
-    tkinter.ttk.Label(frm, text="Hello World!").grid(column=0, row=0)
-    tkinter.ttk.Button(frm, text="Quit", command=root.destroy).grid(column=1, row=0)
-    root.mainloop()
+class TrackerForm:
+    """
+    The pop-up box for the tracker.
+    """
+    def __init__(self, date_time: datetime.datetime, interval: int, action: Callable):
+        """
+        Create the form handler.
+        """
+        self._interval = interval
+        self._date_time = date_time
+        self._action = action
+        self._width = 350
+        self._height = 150
+        self._root: tkinter.Tk | None = None
+
+    @property
+    def date_time(self) -> str:
+        """
+        Return the pop-up datetime in the hh:mm format.
+        """
+        return self._date_time.strftime("%H:%M")
+
+    def action_wrapper(self) -> None:
+        """
+        Wrap the action so that we can schedule the next event when it's called.
+        """
+        self._action()
+        self._root.destroy()
+
+    def create_text_box_frame(self, master: tkinter.Misc, label_text: str) -> tkinter.Frame:
+        """
+        Generate a frame that has a label and a textbox.
+        """
+        frame = tkinter.Frame(master)
+        label = tkinter.Label(
+            master=frame,
+            text=label_text,
+            width=8,  # round(self._width * 0.1)
+            borderwidth=2,
+            justify="right",
+            background="white",
+            relief="flat",
+        )
+        text_box = tkinter.Text(
+            master=frame,
+            height=1,
+            width=30,  # round(self._width * 0.8)
+            borderwidth=2,
+        )
+
+        label.pack(in_=frame, side=tkinter.LEFT)
+        text_box.pack(in_=frame, side=tkinter.RIGHT)
+        frame.pack(in_=master)
+
+        return frame
+
+    def generate_form(self) -> None:
+        """
+        Generate the tracker pop-up form.
+        """
+        self._root = tkinter.Tk()
+        self._root.geometry(f"{self._width}x{self._height}")
+        self._root.title(f"Interval Tracker at {self.date_time} ({self._interval})")
+
+        text_box_frame = tkinter.LabelFrame(
+            self._root,
+            text="Current Task Details",
+            borderwidth=2,
+            background="white",
+        )
+        text_box_frame.pack(
+            in_=self._root,
+            fill="both",
+            expand=tkinter.YES,
+            side=tkinter.TOP,
+        )
+
+        button_frame = tkinter.Frame(
+            self._root,
+            borderwidth=15,
+        )
+        button_frame.pack(
+            in_=self._root,
+            side=tkinter.BOTTOM,
+            fill=tkinter.BOTH,
+            expand=True,
+        )
+
+        self.create_text_box_frame(master=text_box_frame, label_text="Project")
+        self.create_text_box_frame(master=text_box_frame, label_text="Detail")
+
+        okay_button = tkinter.Button(
+            self._root,
+            height=2,
+            width=20,
+            borderwidth=3,
+            text="OK",
+            command=self.action_wrapper,
+        )
+        okay_button.pack(
+            in_=button_frame,
+            side=tkinter.LEFT,
+        )
+
+        cancel_button = tkinter.Button(
+            self._root,
+            height=2,
+            width=20,
+            borderwidth=3,
+            text="Cancel",
+            command=lambda: self._root.destroy(),
+        )
+        cancel_button.pack(
+            in_=button_frame,
+            side=tkinter.RIGHT,
+        )
+
+        self._root.mainloop()
--- main.py
+++ main.py
@@ -2,17 +2,42 @@
 Entry point into this project.
 """
 import datetime
+import os
 
-import daily_tracker.scheduler
+import dotenv
+
+import daily_tracker.actions
 import daily_tracker.database.database
 import daily_tracker.form
+import daily_tracker.scheduler
+import daily_tracker.calendars.outlook_connector
+
+
+dotenv.load_dotenv(dotenv_path=r".env")
+JIRA_CREDENTIALS = {
+    "url": os.getenv("JIRA_URL"),
+    "key": os.getenv("JIRA_KEY"),
+    "secret": os.getenv("JIRA_SECRET"),
+}
+
+
+def ok_action() -> None:
+    """
+    Dummy action for the pop-up.
+    """
+    print(datetime.datetime.now())
 
 
 def test_action() -> None:
     """
     Dummy action for the scheduler.
     """
-    print(datetime.datetime.now())
+    pop_up = daily_tracker.form.TrackerForm(
+        date_time=datetime.datetime.now(),
+        interval=15,
+        action=ok_action,
+    )
+    pop_up.generate_form()
 
 
 def main() -> None:
@@ -22,10 +47,16 @@ def main() -> None:
     # db_conn = daily_tracker.database.database.DatabaseConnector(filepath='tracker.db')
     # scheduler = daily_tracker.scheduler.IndefiniteScheduler(
     #     action=test_action,
-    #     interval=1
+    #     interval=1,
     # )
     # scheduler.schedule_first()
-    daily_tracker.form.main()
+    # action_handler = daily_tracker.actions.ActionHandler(conn=db_conn)
+    # items = action_handler.get_project_drop_down_list()
+    # print(items)
+    # jira_handler = daily_tracker.actions.JiraHandler(**JIRA_CREDENTIALS)
+    # print(jira_handler.get_tickets_in_sprint())
+    outlook_conn = daily_tracker.calendars.outlook_connector.OutlookConnector()
+    outlook_conn.main()
 
 
 if __name__ == "__main__":
