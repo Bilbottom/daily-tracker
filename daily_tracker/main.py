6f7fd2ef4add9ac3508875f110b3851290200e04
Bill <Bill@TheWallisFamily.co.uk>
2022-11-20 14:52:58 +0000

    Add option to import history

--- daily_tracker/main.py
+++ daily_tracker/main.py
@@ -6,6 +6,10 @@ import pathlib
 
 import daily_tracker.form
 import daily_tracker.scheduler
+import daily_tracker.handlers
+
+
+APPLICATION_CREATED = True
 
 
 def create_env() -> None:
@@ -32,5 +36,10 @@ def main() -> None:
     """
     Entry point into this project.
     """
-    scheduler = daily_tracker.scheduler.IndefiniteScheduler(create_form)
-    scheduler.schedule_first()
+    if APPLICATION_CREATED:
+        scheduler = daily_tracker.scheduler.IndefiniteScheduler(create_form)
+        scheduler.schedule_first()
+    else:
+        create_env()
+        db_handler = daily_tracker.handlers.DatabaseHandler("tracker.db")
+        db_handler.import_history("daily-tracker-data.csv")
