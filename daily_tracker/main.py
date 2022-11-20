3fc3c7cd36830587d73a69d219fc67ab250bee33
Bill <Bill@TheWallisFamily.co.uk>
2022-11-20 14:34:16 +0000

    Add a function to create the `.env` file

--- daily_tracker/main.py
+++ daily_tracker/main.py
@@ -2,9 +2,23 @@
 Connect the various subpackages throughout the project to couple up the objects.
 """
 import datetime
+import pathlib
 
 import daily_tracker.form
 import daily_tracker.scheduler
+
+
+def create_env() -> None:
+    """
+    Create the .env file.
+    """
+    filepath = ".env"
+    if pathlib.Path(filepath).exists():
+        return None
+
+    with open(filepath, "w+") as f:
+        keys = ["JIRA_URL", "JIRA_KEY", "JIRA_SECRET", "SLACK_URL"]
+        f.write("\n".join([f"{key}=" for key in keys]))
 
 
 def create_form(at_datetime: datetime.datetime) -> None:
