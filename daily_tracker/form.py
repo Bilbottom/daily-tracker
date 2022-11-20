9359d180aff9d5de3b6aebdf2a1fd8fa0b477c9e
Bill <Bill@TheWallisFamily.co.uk>
2022-11-20 15:11:47 +0000

    Tweak options to be suitable for production

--- daily_tracker/form.py
+++ daily_tracker/form.py
@@ -20,7 +20,7 @@ STYLE = {
 ICON = "daily_tracker/clock-icon.png"
 
 
-def load_icon(filepath: str) -> PIL.ImageTk.PhotoImage:
+def load_icon(filepath: str) -> PIL.ImageTk.PhotoImage | PIL.Image.Image:
     """
     Load an image to the ICO format so that it can be used as application icons.
     """
